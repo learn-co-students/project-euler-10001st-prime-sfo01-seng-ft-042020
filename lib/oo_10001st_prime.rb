c8c1068ffd6bc24f2df110321f2a6d18fb7c8fcb
Don Mallory <dmm333@gmail.com>
2020-08-22 16:01:01 -0500

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,22 @@
-# Implement your object-oriented solution here!
+class Prime
+    attr_reader :number
+
+    def initialize(n)
+        @number = 1
+        num_primes = 1
+        loop do
+            @number += 2
+            if is_prime?(@number)
+                num_primes += 1
+                return if num_primes == n
+            end
+        end
+        
+        def is_prime?(num)
+            for i in 2...num
+                return false if num % i == 0
+            end
+            return true
+        end
+    end
+end
